d092e98f5e7f3b615203896a849a6d76dba59d31
kskim5958 <kskim5958@gmail.com>
2026-01-10 21:13:01 +0900

    update

--- php/admin/db_select.php
+++ php/admin/db_select.php
@@ -2,15 +2,17 @@
 <div id="content__list">
     <section class="content user__table">
         <table>
-            <tr>
-                <th>순번</th>
-                <th>상태</th>
-                <th>이름</th>
-                <th>연락처</th>
-                <th>유입경로</th>
-                <th>유입위치</th>
-                <th>리콜상태</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th>순번</th>
+                    <th>상태</th>
+                    <th>이름</th>
+                    <th>연락처</th>
+                    <th>유입경로</th>
+                    <th>유입위치</th>
+                    <th>리콜상태</th>
+                </tr>
+            </thead>
             <tbody>
                 <tr>
                     <td>순번</td>
@@ -19,7 +21,28 @@
                     <td>연락처</td>
                     <td>유입경로</td>
                     <td>유입위치</td>
-                    <td>리콜상태</td>
+                    <td>
+                        <ul class="text__group">
+                            <li class="text--small text--orange">리콜추가</li>
+                            <li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>
+                            <li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>
+                            <li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="7" class="insert__form">
+                        <select name="" id="">
+                            <option value="">option type 01</option>
+                            <option value="">option type 01</option>
+                            <option value="">option type 01</option>
+                            <option value="">option type 01</option>
+                            <option value="">option type 01</option>
+                        </select>
+                        <input type="text">
+                        <button type="button">추가하기</button>
+                        <button type="button">닫기</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
--- php/main/herder.php
+++ php/main/herder.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>information/dr</title>
     <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="/css/default.css">
     <link rel="stylesheet" href="/css/main-content.css">
+    <link rel="stylesheet" href="/css/admin.css">
     <link rel="stylesheet" href="/css/swiper.css">
 </head>
 <body>
