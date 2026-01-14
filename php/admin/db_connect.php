db6c4a13589a96285b458c93a0e2fc120aa4a6a3
kskim5958 <kskim5958@gmail.com>
2026-01-14 19:01:03 +0900

    update

---
+++ php/admin/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'yeppeum.kr';
+// $host = 'localhost';
+$username = 'kskim5958';
+$password = 'rhkdtjr77';
+$database = 'kskim5958';
+$conn = mysqli_connect($host, $username, $password, $database);
+if (mysqli_connect_errno()) {
+    echo '서버와 연결실패 : ' . mysqli_connect_error();
+}
+?>
