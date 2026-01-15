4594628b4ed0a996241c7c32b91e52bd74cb43dc
kskim5958 <kskim5958@gmail.com>
2026-01-15 15:20:57 +0900

    update

--- js/admin.js
+++ js/admin.js
@@ -3,11 +3,14 @@ $('.user__table .form_open, .recall__form .close').on('click', function() {
     $('.recall__row[data-id="' + user_id + '"]').toggle();
 });
 
+$('.recall__column .recall_list_open').on('click', function() {
+    $(this).parent().children('.hide').toggle();
+});
+
 $('.recall__form .insert').on('click', function() {
     let user_id = $(this).attr('data-id');
     let element = $('.recall__form[data-id="' + user_id + '"]');
     let status = element.find('select[name="status"] option:selected').val();
     let comment = element.find('input[name="comment"]').val();
-    // comment에 값이 있으면 status(7)를 기타로 변경
-    if (comment == '') status = 7;
+    if (comment != '') status = 999;
 });
--- php/admin/user_table.php
+++ php/admin/user_table.php
@@ -32,26 +32,35 @@ include 'db_connect.php';
                     . '<td name="path">' . $row['path'] . '</td>'
                     . '<td name="ip">' . $row['ip'] . '</td>'
                     . '<td>'
-                    . '<ul class="recall__column">'
-                    . '<li class="form_open" data-id="' . $row['no'] . '">리콜추가</li>'
-                    . '<li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>'
-                    . '<li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>'
-                    . '<li>2026-01-08 12:56 카카오톡 발송&nbsp;<span class="text--small text--orange">삭제</span></li>'
-                    . '</ul>'
-                    . '</td>'
-                    . '</tr>'
-                    . '<tr class="recall__row" data-id="' . $row['no'] . '">'
+                    . '<ul class="recall__column">';
+                    // . '<li class="form_open" data-id="' . $row['no'] . '">리콜추가</li>';
+                    // $sql = 'SELECT  * FROM RECALL WHERE status = 0 AND userNo = ' . $row['no'] . ' ORDER BY recallDate DESC;';
+                    // $result_r = mysqli_query($conn, $sql);
+                    // $count_r = mysqli_num_rows($result_r);
+                    // if ($count_r != 0) {
+                    //     $index = 1;
+                    //     while ($row_r = mysqli_fetch_array($result_r)) {
+                    //         $index > 3 ? $class_r = 'class="hide"' : $class_r = '';
+                    //         echo '<li ' . $class_r . '>'
+                    //         . $row_r['recallDate'] . '&nbsp;'
+                    //         . $row_r['comment'] . '&nbsp;'
+                    //         .'<span class="del" data-id="' . $row_r['no'] . '" data-userid="' . $row_r['userNo'] . '">삭제</span>'
+                    //         .'</li>';
+                    //         $index++;
+                    //     }
+                    //     if ($count_r > 3) echo '<li class="recall_list_open" data-id="' . $row['no'] . '">더보기</li>';
+                    // }
+                    echo '</ul></td></tr>';
+                    echo '<tr class="recall__row" data-id="' . $row['no'] . '">'
                     . '<td colspan="8">'
                     . '<div class="recall__form" data-id="' . $row['no'] . '">'
-                    . '<select name="status">'
-                    . '<option value="0">예약완료</option>'
-                    . '<option value="1">전화부재</option>'
-                    . '<option value="2">전화돌림</option>'
-                    . '<option value="3">나중에 전화주세요.</option>'
-                    . '<option value="4">직접 전화주신데요.</option>'
-                    . '<option value="5">문자 보내드렸어요.</option>'
-                    . '<option value="6">카카오톡 보내드렸어요.</option>ㅍ'
-                    . '</select>'
+                    . '<select name="status">';
+                    $sql ='SELECT * FROM RECALL_STATUS ORDER BY no ASC';
+                    $result_r_s = mysqli_query($conn, $sql);
+                    while ($row_r_s = mysqli_fetch_array($result_r_s)) {
+                        echo '<option value="' . $row_r_s['no'] . '">' . $row_r_s['comment'] . '</option>';
+                    }
+                    echo '</select>'
                     . '<input name="comment" type="text" placeholder="기타 : 내용">'
                     . '<button type="button" class="insert" data-id="' . $row['no'] . '">추가하기</button>'
                     . '<button type="button" class="close" data-id="' . $row['no'] . '">닫기</button>'
@@ -59,9 +68,19 @@ include 'db_connect.php';
                     . '</td>'
                     . '</tr>';
                 }
+                $recallArr = array();
+                $sql = 'SELECT  * FROM RECALL ORDER BY recallDate DESC;';
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_array($result)) {
+                    $recallArr[] = [$row['no'] => array('userNo' => $row['userNo'], 'recallDate' => $row['recallDate']), 'comment' => $row['comment']];
+                }
                 ?>
             </tbody>
         </table>
     </section>
 </div>
+<script>
+    var recallArr = '<?php echo json_encode($recallArr); ?>';
+    console.log('recallArr : ' + recallArr);
+</script>
 <?php include $_SERVER["DOCUMENT_ROOT"] . '/php/main/footer.php'; ?>
