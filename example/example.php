aa77c1abec7247ad2dc946246170a21f64eb6d62
kskim5958 <kskim5958@gmail.com>
2026-01-18 03:33:29 +0900

    가변배열 예제

--- example/example.php
+++ example/example.php
@@ -1,21 +1,69 @@
 <?php
-$mysqli = @new mysqli('yeppeum.kr', 'kskim5958', 'rhkdtjr77', 'kskim5958');
-if($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
+function fn_example($arr=[]) {
+    $cnt = count($arr);
+    $str = '';
+    if ($cnt != 0) {
+        $index = 1;
+        foreach ($arr as $key => $value) {
+            $str .= " $key = $value ";
+            $str .= ($cnt > 1 && $cnt != $index) ? "AND" : "";
+            $index++;
+        }
+    }
+    return $str;
 }
-$sql = 'SELECT * FROM  VISIT ORDER BY no DESC;';
-$result = $mysqli->query($sql, MYSQLI_STORE_RESULT);
+$arr = ['no'=>1000, 'userName'=>'kim', 'comment'=>'content comment'];
+echo fn_example($arr);
 
-$result->free_result();
-$mysqli->close();
+# mysqli 예제
+// $mysqli = new mysqli('yeppeum.kr', 'kskim5958', 'rhkdtjr77', 'kskim5958');
+// if($mysqli->connect_errno) {
+//     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+//     exit();
+// }
+// $sql = 'SELECT * FROM  VISIT ORDER BY no DESC;';
+// $result = $mysqli->query($sql, MYSQLI_STORE_RESULT);
 
-# key value 형태
+// echo "Affected rows: " . $mysqli -> affected_rows;
+
+
+// $mysqli->close();
+
+// # key value 형태
 // while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
 //     printf ("%s (%s)\n", $row["no"], $row["userName"]);
 // }
-# 단순 배열형태
+// $result->free_result();
+// # 단순 배열형태
 // while ($row = $result -> fetch_array(MYSQLI_NUM)) {
 //     printf ("%s (%s)\n", $row[0], $row[1]);
 // }
-// echo "Affected rows: " . $mysqli -> affected_rows;
+# ALTER TABLE COMMENT_TYPE MODIFY COLUMN no INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+// $id = 1; // 업데이트할 레코드 ID
+// $new_name = "New Name";
+
+// // 2. UPDATE 쿼리 실행 (Prepared Statement)
+// // si는 s는 문자열 i는 정수
+// $stmt = $conn->prepare("UPDATE users SET name = ? WHERE id = ?");
+// $stmt->bind_param("si", $new_name, $id);
+// $stmt->execute();
+
+// // 3. 업데이트 성공 확인 (선택 사항)
+// if ($stmt->affected_rows > 0) {
+//     // 4. 업데이트된 데이터 가져오기
+//     $select_stmt = $conn->prepare("SELECT name, updated_at FROM users WHERE id = ?");
+//     $select_stmt->bind_param("i", $id);
+//     $select_stmt->execute();
+//     $result = $select_stmt->get_result();
+//     $row = $result->fetch_assoc();
+    
+//     echo "업데이트 성공! 변경된 이름: " . $row['name'];
+//     $select_stmt->close();
+// }
+
+// $stmt->close();
+// $conn->close();
+
+# 글로벌변수 힌트
+// /** @var mysqli $mysqli */
