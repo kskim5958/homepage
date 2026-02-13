2202a0a4f23d342c76da4ac7cf052916090ff73c
kskim5958 <kskim5958@gmail.com>
2026-02-13 16:44:39 +0900

    수정중

--- php/excel/excel.php
+++ php/excel/excel.php
@@ -1,19 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>엑셀파일읽기</title>
-</head>
+require ' C:\\Users\\desk01\\Desktop\\homepage\\vendor\\autoload.php';
+// use PhpOffice\PhpSpreadsheet\IOFactory;
 
-<body>
-    <form name="add_form_entry" id="add_form_entry" method="post" action="excel_file_read.php"
-        enctype="multipart/form-data">
-        <label for="inputFileName">Select a file:</label>
-        <input type="file" name="inputFileName" size="40">
-        <input type="submit" value="확인">
-    </form>
-</body>
+// // 엑셀 파일 경로
+// $inputFileName = './sample.xlsx';
 
-</html>
+// // 엑셀 파일 타입에 맞춰 자동으로 리더 생성
+// $spreadsheet = IOFactory::load($inputFileName);
+
+// // 첫 번째 시트 선택
+// $sheet = $spreadsheet->getActiveSheet();
+
+// // 전체 데이터를 배열로 변환
+// $data = $sheet->toArray();
+
+// // 데이터 출력
+// foreach ($data as $row) {
+//     print_r($row);
+// }
+
+// echo getcwd();
