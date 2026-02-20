101eb4a73a2aa5801f1979dea9386d689e13f8f9
kskim5958 <kskim5958@gmail.com>
2026-02-20 18:49:52 +0900

    엑셀 수정중(3)

--- js/payment.js
+++ js/payment.js
@@ -7,16 +7,68 @@ $('[name="upload_btn"]').click(function (e) {
     form_data.append('file', file_data);
 
     $.ajax({
-        url: "/php/excel/upload.php", // 서버측 PHP 파일
+        url: "/php/excel/upload.php",
         type: "POST",
         data: form_data,
-        contentType: false, // 필수
-        processData: false, // 필수
+        contentType: false,
+        processData: false,
         success: function (data) {
-        $("#result_area").html(data); // 새로고침 없이 내용 갱신
+            data = JSON.parse(data);
+            const result = data.result;
+            if (result) {
+                const list = data.list;
+                const today = data.today;
+                const html = fn_payment_list_html(list, today);
+                $("#result_area").append(html);
+            }
         },
         error: function () {
-        alert("파일 읽기 실패");
+            alert("파일 읽기 실패");
         },
     });
 });
+
+const fn_payment_list_html = (list, today) => {
+    let html = "";
+    html += "<table>";
+    html += 
+        `<thead>
+            <tr>
+                <th colspan="12">${today}</th>
+            </tr>
+            <tr>
+                <th rowspan="2">순번</th>
+                <th rowspan="2" colspan="2">성명</th>
+                <th colspan="3">보험수납</th>
+                <th colspan="3">비보험수납</th>
+                <th rowspan="2">합계</th>
+                <th rowspan="2">진료내역</th>
+            </tr>
+            <tr>
+                <th>현금</th>
+                <th>카드</th>
+                <th>계좌이체</th>
+                <th>현금</th>
+                <th>카드</th>
+                <th>계좌이체</th>
+            </tr>
+        </thead>`;
+    html += "<tbody>";
+    $.each(list, function(index, arr) {
+        html += "<tr>";
+        html += `<td>${index}</td>`;
+        html += `<td colspan="2">${arr.name}</td>`;
+        html += `<td>${arr.ins_card}</td>`;
+        html += `<td>${arr.ins_cash}</td>`;
+        html += `<td>${arr.ins_online}</td>`;
+        html += `<td>${arr.unins_card}</td>`;
+        html += `<td>${arr.unins_cash}</td>`;
+        html += `<td>${arr.unins_online}</td>`;
+        html += `<td>${arr.payment_sum}</td>`;
+        html += `<td>${arr.content}</td>`;
+        html += "</tr>";
+    });
+    html += "</tbody>";
+    html += "</table>";
+    return html;
+}
--- php/excel/upload.php
+++ php/excel/upload.php
@@ -6,23 +6,79 @@ if ($_FILES['file']['name']) {
     $file_tmp = $_FILES['file']['tmp_name'];
     
     try {
-        // 엑셀 파일 로드
-        $spreadsheet = IOFactory::load($file_tmp);
+        $spreadsheet = IOFactory::load($file_tmp); // 엑셀 파일 로드
         $sheet = $spreadsheet->getSheet(1);
         $data = $sheet->toArray(); // 엑셀 데이터를 배열로 변환
 
-        // HTML 테이블 생성
-        echo '<table border="1">';
-        foreach ($data as $row) {
-            echo '<tr>';
-            foreach ($row as $cell) {
-                echo '<td>' . htmlspecialchars($cell) . '</td>';
-            }
-            echo '</tr>';
+        $r_row = [];
+        foreach ($data as $index => $row) {
+            if ($index == 0) continue;
+            else $r_row[] = $row;
         }
-        echo '</table>';
-
+        if (count($r_row) != 0) {
+            foreach ($r_row as $index => $value) {
+                $name = $r_row[$index][2] . " " . $r_row[$index][3];
+                $card = num($r_row[$index][17]);
+                $cash = num($r_row[$index][18]);
+                $online = num($r_row[$index][19]);
+                $unins = num($r_row[$index][13]);
+                $ins = ($card + $cash + $online) - $unins;
+                $payment_ls[$index] = [
+                    "name" => $name,
+                    "card" => $card,
+                    "cash" => $cash,
+                    "online" => $online,
+                    "ins" => $ins,
+                    "unins" => $unins,
+                    "ins_card" => 0,
+                    "ins_cash" => 0,
+                    "ins_online" => 0,
+                    "unins_card" => 0,
+                    "unins_cash" => 0,
+                    "unins_online" => 0
+                ];
+                if ($ins > 0) {
+                    $var = 0;
+                    if ($card > $var) {
+                        $var = $card - $ins;
+                        $payment_ls[$index]["ins_card"] = ($ins >= $card) ? $card : $ins;
+                    }
+                    if ($cash > $var) {
+                        $var = $cash - $ins;
+                        $payment_ls[$index]["ins_cash"] = ($ins >= $cash) ? $cash : $ins;
+                    }
+                    if ($online > $var) {
+                        $var = $online - $ins;
+                        $payment_ls[$index]["ins_online"] = ($ins >= $online) ? $online : $ins;
+                    }
+                }
+                if ($unins > 0) {
+                    $var = 0;
+                    if ($card > $var) {
+                        $var = $card - $unins;
+                        $payment_ls[$index]["unins_card"] = ($unins >= $card) ? $card : $unins;
+                    }
+                    if ($cash > $var) {
+                        $var = $cash - $unins;
+                        $payment_ls[$index]["unins_cash"] = ($unins >= $cash) ? $cash : $unins;
+                    }
+                    if ($online > $var) {
+                        $var = $online - $unins;
+                        $payment_ls[$index]["unins_online"] = ($unins >= $online) ? $online : $unins;
+                    }
+                }
+                $payment_ls[$index]["payment_sum"] = $card + $cash + $online;
+                $payment_ls[$index]["content"] = $r_row[$index][25];
+            }
+        }
+        $json = json_encode(["result" => true, "list" => $payment_ls, "today" => $r_row[0][1]]);
     } catch (Exception $e) {
-        echo 'Error: ' . $e->getMessage();
+        $json = json_encode(["result" => false, "error" => $e->getMessage()]);
     }
+    echo $json;
+}
+
+function num($val) {
+    $result = preg_replace("/[^0-9-]/", "", $val);
+    return (int)$result;
 }
