b737d1897c7f69842545dd1aa021ea4c955d24eb
kskim5958 <kskim5958@gmail.com>
2026-02-25 23:22:50 +0900

    상담자료 수정중(2)

--- js/healing.js
+++ js/healing.js
@@ -3,25 +3,43 @@ $(document).ready(function (e) {
 });
 
 $(document).on('click', '.teeth', function(e) {
-    const teeth_arr = {basin : 0, resin : 1, inlay : 2, crown : 3, implant : 4, panic : 5, ext : 6};
-    const teeth_id = $(this).attr("id");
-    const teeth_val = Number($(this).data("value"));
-    const el = $(`#${teeth_id}`).children("img");
-    const root = el.attr("src");
-    let root_arr = root.split("/");
-    root_arr = $.grep(root_arr, function(value) {
+    const teeth_arr = {
+      basic: 0,
+      resin: 1,
+      inlay: 2,
+      crown: 3,
+      implant: 4,
+      panic: 5
+    };
+    let teeth_id = Number($(this).attr("id"));
+    let teeth_value = Number($(this).attr("data-value")) + 1;
+    let teeth_key = getKeyByValue(teeth_arr, teeth_value);
+    const teeth_el = $(`#${teeth_id}`);
+    const current_src = teeth_el.children("img").attr("src");
+    let current_src_arr = current_src.split("/");
+    current_src_arr = $.grep(current_src_arr, function(value) {
         return value !== "";
     });
-    const last_index = root_arr.length - 1;
-    root_arr.splice(last_index, 1);
-    let img_src = "";
-    root_arr.forEach(str => {
-        img_src += "/" + str;
+    const last_index = current_src_arr.length - 1;
+    current_src_arr.splice(last_index, 1);
+    let chang_src = "";
+    current_src_arr.forEach(str => {
+        chang_src += "/" + str;
     });
-    img_src += "/" + teeth_id + ".png";
-    $.each(teeth_arr, function(key, value) {
-        console.log(key + ": " + value);
-    });
+
+    if (teeth_id >= 21 && teeth_id <= 28) {
+      teeth_id = teeth_id - 10;
+    } else if (teeth_id >= 31 && teeth_id <= 38) {
+      teeth_id = teeth_id + 10;
+    }
+
+    if (teeth_value == 6) {
+        teeth_value = 0;
+        teeth_key = "basic";
+    }
+    chang_src += `/${teeth_id}_${teeth_key}.png`;
+    teeth_el.attr("data-value", teeth_value);
+    teeth_el.children("img").prop("src" , chang_src);
 });
 
 const fn_teeth_html = () => {
@@ -35,7 +53,7 @@ const fn_teeth_html = () => {
         upper_left_html += `<div id="${teeth_num}" class="teeth${class_el}" data-value="0"><img src="/img/teeth/upper/${teeth_num}_basic.png" alt=""></div>`;
         upper_right_html += `<div id="${teeth_num + 10}" class="teeth${class_el}" data-value="0"><img ${class_el} src="/img/teeth/upper/${teeth_num}_basic.png" alt=""></div>`;
     }
-    for (let i = 1; i <= 8; i++) {
+    for (let i = 8; i >= 1; i--) {
         const teeth_num = i + 40;
         const class_el = (i >= 6) ? " molar" : "";
         lower_left_html += `<div id="${teeth_num}" class="teeth${class_el}" data-value="0"><img ${class_el} src="/img/teeth/lower/${teeth_num}_basic.png" alt=""></div>`;
@@ -45,4 +63,8 @@ const fn_teeth_html = () => {
     $('#teeth').find('#upper').find('.right').append(upper_right_html);
     $('#teeth').find('#lower').find('.left').append(lower_left_html);
     $('#teeth').find('#lower').find('.right').append(lower_right_html);
+}
+
+const getKeyByValue = (obj, value) => {
+    return Object.keys(obj).find(key => obj[key] === value);
 }
